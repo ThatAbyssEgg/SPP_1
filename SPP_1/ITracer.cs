f0b301e5de83b5fa3d6f1d721cfadf46bf973411
ThatAbyssEgg <probablynotfaputa@gmail.com>
2022-11-01 00:40:07 +0300

    Add project files.

---
+++ SPP_1/ITracer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPP_1
+{
+    public struct TraceResult
+    {
+
+    }
+
+    public interface ITracer
+    {
+        void StartTrace();
+
+        void StopTrace();
+
+        TraceResult GetTraceResult();
+
+    }
+}
---
+++ SPP_1/MethodTrace.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPP_1
+{
+    public class MethodTrace
+    {
+        private readonly ITracer _tracer;
+        private readonly CriticalRegister _criticalHit;
+
+        internal MethodTrace(ITracer tracer)
+        {
+            _tracer = tracer;
+            _criticalHit = new CriticalRegister(_tracer);
+        }
+
+        public void HitCounter()
+        {
+            _tracer.StartTrace();
+
+            Console.WriteLine("Input the amount of d20s you'd like to roll.");
+            
+            
+        }
+    }
+
+    public class CriticalRegister
+    {
+        private readonly ITracer _tracer;
+        internal CriticalRegister(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void CriticalSuccess()
+        {
+            Console.WriteLine("Boom! You hit a twenty! What a lucky shot!");
+        }
+
+        public void CriticalFailure()
+        {
+            Console.WriteLine("Aww, you got -one-... Such a disappointment!");
+        }
+    }
+}
