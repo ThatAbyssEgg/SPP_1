dfb923504d20009d88f777ba1ccaf3c476de5236
ThatAbyssEgg <probablynotfaputa@gmail.com>
2022-11-01 03:34:21 +0300

    Added a few sound effects & completed the main- and sub-classes

--- SPP_1/MethodTrace.cs
+++ SPP_1/MethodTrace.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 
+
+// All the sounds were taken from freesound.org
+    
 namespace SPP_1
 {
     public class MethodTrace
     {
+
+        private const int MAX_DICE_VALUE = 20;
+        private const int MIN_DICE_VALUE = 1;
         private readonly ITracer _tracer;
-        private readonly CriticalRegister _criticalHit;
+        private readonly CriticalRegister _criticalRegister;
 
         internal MethodTrace(ITracer tracer)
         {
             _tracer = tracer;
-            _criticalHit = new CriticalRegister(_tracer);
+            _criticalRegister = new CriticalRegister(_tracer);
         }
 
         public void HitCounter()
@@ -22,8 +29,25 @@ namespace SPP_1
             _tracer.StartTrace();
 
             Console.WriteLine("Input the amount of d20s you'd like to roll.");
-            
-            
+            int diceRolls = Convert.ToInt32(Console.ReadLine());
+
+            int currentRoll;
+            Random random = new Random();
+            for (int i = 0; i < diceRolls; i++)
+            {
+                currentRoll = random.Next(MIN_DICE_VALUE, MAX_DICE_VALUE);
+
+                if (currentRoll == MAX_DICE_VALUE)
+                {
+                    _criticalRegister.CriticalSuccess();
+                }
+                else if (currentRoll == MIN_DICE_VALUE)
+                {
+                    _criticalRegister.CriticalFailure();
+                }
+            }
+
+
         }
     }
 
@@ -37,12 +61,31 @@ namespace SPP_1
 
         public void CriticalSuccess()
         {
+            if (OperatingSystem.IsWindows())
+            {
+                SoundPlayer soundPlayer = new SoundPlayer("success.wav");
+                soundPlayer.Load();
+                soundPlayer.PlaySync();
+            }
             Console.WriteLine("Boom! You hit a twenty! What a lucky shot!");
+            
         }
 
         public void CriticalFailure()
         {
-            Console.WriteLine("Aww, you got -one-... Such a disappointment!");
+            if (OperatingSystem.IsWindows())
+            {
+                SoundPlayer soundPlayer = new SoundPlayer("failure.wav");
+                soundPlayer.Load();
+                soundPlayer.PlaySync();
+            }
+            Console.WriteLine("Aww, you got one... Such a disappointment!");
         }
+    }
+
+    public static class Main
+    {
+        MethodTrace methodTrace = new MethodTrace();
+        MethodTrace.HitCounter();
     }
 }
